811f3d3eb83017973b3d101efaeb803d8e7c6cd9
ThangND <nguyenducthang@Nguyens-MacBook-Pro.local>
2025-06-19 13:03:29 +0900

    feat: auth

---
+++ lib/apis/auth_api.dart
@@ -0,0 +1,77 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as model;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:twitter_clone/core/core.dart';
+import 'package:twitter_clone/core/providers.dart';
+
+// want to signup, want to get user account -> Account
+// want to access user related data -> model.User
+
+final authAPIProvider = Provider((ref) {
+  final account = ref.watch(appwriteAccountProvider);
+  return AuthAPI(account: account);
+});
+
+abstract class IAuthAPI {
+  FutureEither<model.User> signUp({
+    required String email,
+    required String password,
+  });
+
+  FutureEither<model.Session> login({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthAPI implements IAuthAPI {
+  final Account _account;
+  AuthAPI({required Account account}) : _account = account;
+
+  @override
+  FutureEither<model.User> signUp({
+    required String email,
+    required String password,
+  }) async {
+    // model.User.
+    try {
+      final account = await _account.create(
+        userId: ID.unique(),
+        email: email,
+        password: password,
+      );
+      return right(account);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+        Failure(e.message ?? 'Some unexpected error occurred', stackTrace),
+      );
+    } catch (e, stackTrace) {
+      return left(
+        Failure(e.toString(), stackTrace),
+      );
+    }
+  }
+
+  @override
+  FutureEither<model.Session> login({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final account = await _account.createEmailPasswordSession(
+        email: email,
+        password: password,
+      );
+      return right(account);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+        Failure(e.message ?? 'Some unexpected error occurred', stackTrace),
+      );
+    } catch (e, stackTrace) {
+      return left(
+        Failure(e.toString(), stackTrace),
+      );
+    }
+  }
+}
---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '67fe21d800113191ceeb';
+  static const String projectId = '67fe108000353aa3510d';
+  static const String endPoint = 'http://localhost:80/v1';
+}
